f51780649a5fad9ccf16a3d9b472684ef976cd87
A-Smith-01 <alex3.141smith@gmail.com>
2025-08-20 11:40:28 +0100

    Implemented ship module and passed all tests

--- src/ship.js
+++ src/ship.js
@@ -1,5 +1,18 @@
-function Ship(){
-    
+function Ship(length){
+    let hits = 0
+    function hit(){
+        hits ++;
+    }
+
+    function getHits(){
+        return hits
+    }
+
+    function isSunk(){
+        return hits >= length
+    }
+
+    return {length, getHits,hit,isSunk}
 }
 
 module.exports = Ship
--- test/ship.test.js
+++ test/ship.test.js
@@ -3,15 +3,15 @@ const Ship = require("../src/ship")
 test('Testing the hit function',() => {
     const ship = Ship(3)
     ship.hit()
-    expect(ship.hits).toBe(1)
+    expect(ship.getHits()).toBe(1)
     ship.hit()
-    expect(ship.hits).toBe(2)
+    expect(ship.getHits()).toBe(2)
 })
 
 test('The ship should be sunk after hits == length',() => {
     const ship = Ship(3)
     ship.hit()
-    expect(ship.isSunk()).toBe(true)
+    expect(ship.isSunk()).toBe(false)
     ship.hit()
     ship.hit()
     expect(ship.isSunk()).toBe(true)
